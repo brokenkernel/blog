20ce1b83468818dc6c4466c96b511b88b227117e
Eitan Adler <lists@eitanadler.com>
2020-08-20 17:31:23 -0700

    update

--- blog/posts/my-first-post.md
+++ blog/posts/my-first-post.md
@@ -3,3 +3,5 @@ title: "Test Post"
 date: 2020-08-20T17:24:07-07:00
 ---
 
+hello
+
--- publish.sh
+++ publish.sh
@@ -4,10 +4,10 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 rm -rf public/*
 
 hugo
 
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" --no-verify
