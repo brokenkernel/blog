341f11d0d933eb77f68dab14aadf634c8a8f42c9
Eitan Adler <lists@eitanadler.com>
2020-08-20 18:24:48 -0700

    simplify

--- publish.sh
+++ publish.sh
@@ -2,14 +2,6 @@ set -xe
 cd "$(git rev-parse --show-toplevel)"
 cd "blog"
 
-rm -rf public
-mkdir public
-git worktree prune
-
-git worktree add -B gh-pages public origin/gh-pages
-
-rm -rf public/*
-
-hugo
+hugo --verbose
 
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" --no-verify
